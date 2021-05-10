282d63297662a5698ce90b28df38313c4bdb941e
Samuel Brehm <samuelbrehm@gmail.com>
2021-05-10 14:33:35 -0300

    Initial Commit

---
+++ CleanApp/CleanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanAppApp.swift
+//  CleanApp
+//
+//  Created by Samuel Brehm on 10/05/21.
+//
+
+import SwiftUI
+
+@main
+struct CleanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
