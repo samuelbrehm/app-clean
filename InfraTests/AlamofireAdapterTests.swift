f9a446ef46f278eebbb762a60973f68f7ff489cf
Samuel Brehm <samuelbrehm@gmail.com>
2021-05-12 09:58:28 -0300

    move AlamofireAdapter to its own file

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -1,41 +1,7 @@
 import XCTest
 import Alamofire
 import Data
-
-class AlamofireAdapter: HttpPostClient {   
-    private var session: Session
-    
-    init(session: Session = .default) {
-        self.session = session
-    }
-    
-    func post(to url: URL, with data: Data?, completion: @escaping (Result<Data?, HttpError>) -> Void) {
-        let json = data?.toJson()
-        session.request(url, method: .post, parameters: json).responseData { dataResponse in
-            guard let statusCode = dataResponse.response?.statusCode else { return completion(.failure(.noConnectivity)) }
-            switch dataResponse.result {
-            case .failure: completion(.failure(.noConnectivity))
-            case .success(let data):
-                switch statusCode {
-                case 204:
-                    completion(.success(nil))
-                case 200...299:
-                    completion(.success(data))
-                case 401:
-                    completion(.failure(.unauthorized))
-                case 403:
-                    completion(.failure(.forbidden))
-                case 400...499:
-                    completion(.failure(.badRequest))
-                case 500...599:
-                    completion(.failure(.serverError))
-                default:
-                    completion(.failure(.noConnectivity))
-                }
-            }
-        }
-    }
-}
+import Infra
 
 class AlamofireAdapterTests: XCTestCase {
     func test_post_should_make_request_with_valid_url_and_method() {
