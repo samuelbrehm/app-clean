c2dc45d83c0828ab2f3dbaccfea9e634dcbd27f4
Samuel Brehm <samuelbrehm@gmail.com>
2021-05-13 17:10:38 -0300

    move classes to their own files

---
+++ PresentaionTests/Presenters/Views/LoadingViewSpy.swift
@@ -0,0 +1,14 @@
+import Foundation
+import Presentaion
+
+class LoadingViewSpy: LoadingView {
+    var emit: ((LoadingViewModel) -> Void)?
+    
+    func observe(completion: @escaping (LoadingViewModel) -> Void) {
+        self.emit = completion
+    }
+    
+    func display(viewModel: LoadingViewModel) {
+        self.emit?(viewModel)
+    }
+}
