b485169519bd83b867ba25c4aac95baff1b5e0bf
Samuel Brehm <samuelbrehm@gmail.com>
2021-05-15 14:57:48 -0300

    move out creation to a factory method

--- UI-iOSTests/SignUpViewControllerTests.swift
+++ UI-iOSTests/SignUpViewControllerTests.swift
@@ -6,21 +6,26 @@ import Presentaion
 class SignUpViewControllerTests: XCTestCase {
     
     func test_loading_is_hidden_on_start() {
-        let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
-        let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
-        sut.loadViewIfNeeded()
+        let sut = makeSut()
         XCTAssertEqual(sut.loadingIndicator?.isAnimating, false)
     }
     
     func test_sut_implements_loadingView() {
-        let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
-        let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
+        let sut = makeSut()
         XCTAssertNotNil(sut as LoadingView)
     }
     
     func test_sut_implements_alertView() {
+        let sut = makeSut()
+        XCTAssertNotNil(sut as AlertView)
+    }
+}
+
+extension SignUpViewControllerTests {
+    func makeSut() -> SignUpViewController {
         let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
         let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
-        XCTAssertNotNil(sut as AlertView)
+        sut.loadViewIfNeeded()
+        return sut
     }
 }
