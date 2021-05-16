321efcfb95faf3af3fc58c3adc5b821b04b7ca91
Samuel Brehm <samuelbrehm@gmail.com>
2021-05-16 12:41:42 -0300

    update extension UIViewController

--- UI-iOS/Helpers/UIViewControllerExtensions.swift
+++ UI-iOS/Helpers/UIViewControllerExtensions.swift
@@ -3,11 +3,11 @@ import UIKit
 
 extension UIViewController {
     func hideKeyboardOnTap() {
-        let gesture = UIGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         gesture.cancelsTouchesInView = false
         view.addGestureRecognizer(gesture)
     }
-    
+
     @objc private func hideKeyboard() {
         view.endEditing(true)
     }
