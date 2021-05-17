4ea0e9564ecc90555f90fac5deb6c544373f7eb4
Samuel Brehm <samuelbrehm@gmail.com>
2021-05-17 14:33:33 -0300

    move threading login to the decorator class

--- MainTests/Composers/SignUpComposerTests.swift
+++ MainTests/Composers/SignUpComposerTests.swift
@@ -4,15 +4,22 @@ import UI_iOS
 
 class SignUpComposerTests: XCTestCase {
     func test_background_request_shoud_complete_on_main_thread() {
-        let (sut, _) = makeSut()
+        let (sut, addAccountSpy) = makeSut()
         sut.loadViewIfNeeded()
+        sut.signUp?(makeSignUpViewModel())
+        let exp = expectation(description: "waiting")
+        DispatchQueue.global().async {
+            addAccountSpy.completeWithError(.unexpected)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
     }
 }
 
 extension SignUpComposerTests {
     func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (sut: SignUpViewController, addAccountSpy: AddAccountSpy) {
         let addAccountSpy = AddAccountSpy()
-        let sut = SignUpComposer.composeViewControllerWith(addAccount: addAccountSpy)
+        let sut = SignUpComposer.composeViewControllerWith(addAccount: MainQueueDispatchDecorator(addAccountSpy))
         checkMemoryLeak(for: sut, file: file, line: line)
         checkMemoryLeak(for: addAccountSpy, file: file, line: line)
         return (sut, addAccountSpy)
