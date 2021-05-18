2d24a23936f92e6b74c3ebfb12561d0dd6cac33d
Samuel Brehm <samuelbrehm@gmail.com>
2021-05-18 13:55:28 -0300

    ensure RemoteAddAccount completes with emailInUse error when HttpClient completes with forbidden error

--- Domain/Errors/DomainError.swift
+++ Domain/Errors/DomainError.swift
@@ -1,12 +1,6 @@
-//
-//  DomainError.swift
-//  Domain
-//
-//  Created by Samuel Brehm on 11/05/21.
-//
-
 import Foundation
 
 public enum DomainError: Error {
     case unexpected
+    case emailInUse
 }
