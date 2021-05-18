2312b609b3174f2bc4ca7dc35be05a130783637e
Samuel Brehm <samuelbrehm@gmail.com>
2021-05-18 19:37:05 -0300

    ensure RemoteAuthentication calls HttpClient with correct data

--- DataTests/UseCases/RemoteAuthenticationTests.swift
+++ DataTests/UseCases/RemoteAuthenticationTests.swift
@@ -6,8 +6,15 @@ class RemoteAuthenticationTests: XCTestCase {
     func test_add_should_call_httpClient_with_correct_url() throws {
         let url = makeURL()
         let (sut, httpClientSpy) = makeSut(url: url)
-        sut.auth()
+        sut.auth(authenticationModel: makeAuthenticationModel())
         XCTAssertEqual(httpClientSpy.urls, [url])
+    }
+    
+    func test_add_should_call_httpClient_with_correct_data() throws {
+        let (sut, httpClientSpy) = makeSut()
+        let authenticationModel = makeAuthenticationModel()
+        sut.auth(authenticationModel: authenticationModel)
+        XCTAssertEqual(httpClientSpy.data, authenticationModel.toData())
     }
 }
 
