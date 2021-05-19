f36963572211389301cafc1c5539461b2959b9ea
Samuel Brehm <samuelbrehm@gmail.com>
2021-05-19 14:09:27 -0300

    ensure LoginController compose with correct validations

---
+++ Main/Factories/Controllers/LoginControllerFactory.swift
@@ -0,0 +1,21 @@
+import Foundation
+import UI_iOS
+import Presentaion
+import Validation
+import Domain
+
+public func makeLoginController(authentication: Authentication) -> LoginViewController {
+    let controller = LoginViewController.instantiate()
+    let validationComposite = ValidationComposite(validations: makeSignUpValidations())
+    let presenter = LoginPresenter(validation: validationComposite, alertView: WeakVarProxy(controller), authentication: authentication, loadingView: WeakVarProxy(controller))
+    controller.login = presenter.login
+    return controller
+}
+
+public func makeLoginValidations() -> [Validation] {
+    return [
+        RequiredFieldValidation(fieldName: "email", fieldLabel: "Email"),
+        EmailValidation(fieldName: "email", fieldLabel: "Email", emailValidator: makeEmailValidatorAdapter()),
+        RequiredFieldValidation(fieldName: "password", fieldLabel: "Senha"),
+    ]
+}
