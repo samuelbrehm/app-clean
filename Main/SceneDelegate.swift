97118c40e11974d70e5ac057a304ede986d17db9
Samuel Brehm <samuelbrehm@gmail.com>
2021-05-19 17:26:57 -0300

    change NavigationController init

--- Main/SceneDelegate.swift
+++ Main/SceneDelegate.swift
@@ -3,16 +3,15 @@ import UI_iOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        let nav = NavigationController()
         let httpClient = makeAlamofireAdapter()
-//        let addAccount = makeRemoteAddAccount(httpClient: httpClient)
-//        let signUpController = makeSignUpController(addAccount: addAccount)
-        let authentication = makeRemoteAuthentication(httpClient: httpClient)
-        let loginController = makeLoginController(authentication: authentication)
-        let nav = NavigationController(rootViewController: loginController)
+        let addAccount = makeRemoteAddAccount(httpClient: httpClient)
+        let signUpController = makeSignUpController(addAccount: addAccount)
+        nav.setRootViewController(signUpController)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
     }
